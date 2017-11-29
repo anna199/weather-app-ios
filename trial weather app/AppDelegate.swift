96f019f2506ad423303f39497004a78fc3b0bf46
anna199 <liranlr1992@gmail.com>
2017-11-28 21:05:54 -0800

    adding google autocomplete
    adding google autocomplete

--- trial weather app/AppDelegate.swift
+++ trial weather app/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GooglePlaces
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+         GMSServices.provideAPIKey("AIzaSyCc00duaiBKVesT9RhieHGLqq3ayzkQI0E")
+       GMSPlacesClient.provideAPIKey("AIzaSyCc00duaiBKVesT9RhieHGLqq3ayzkQI0E")
         return true
     }
 
